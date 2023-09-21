49dddf1b7ee330857a7472cf280873ba70c195df
mich balkany <askmitchb@gmail.com>
2023-09-21 11:43:32 -0400

    Initial Commit

---
+++ challengeday25/challengeday25App.swift
@@ -0,0 +1,17 @@
+//
+//  challengeday25App.swift
+//  challengeday25
+//
+//  Created by Mich balkany on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct challengeday25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
